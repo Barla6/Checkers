e4413430c7289ea29712cdc78e675b178241bfa9
Barla6 <barlaliko62@gmail.com>
2023-11-14 10:51:55 +0200

    refactor server

--- backend/CheckersServer/src/main/kotlin/com/requestsClasses/NewGameRequestBody.kt
+++ backend/CheckersServer/src/main/kotlin/com/requestsClasses/NewGameRequestBody.kt
@@ -1,7 +1,5 @@
 package com.requestsClasses
 
-import com.checkers.models.GameLevel
-
 data class NewGameRequestBody(val level: String, val playerName: String) : RequestBody() {
     override fun isValid(): Boolean {
         return level != null
